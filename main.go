343ee03049bd4ccad8a707a58495a4ea5caa9bf0
bujosa <davidbujosa@gmail.com>
2022-11-08 20:31:46 -0400

    feat: finish example with only concurrency without parallelism

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// I use this packages because i need to add more go routines
+// So I need to wait for this.
+var wg sync.WaitGroup
+
+func main() {
+	wg.Add(2)
+	go debian() // Call 1
+	go cali() // Call 2
+	wg.Wait()
+}
+
+// Second 
+func debian(){
+	for i := 0; i < 30; i++ {
+		fmt.Println("debian:", i)
+		time.Sleep(time.Duration(1 * time.Millisecond)) // This is for see the concurrency
+	}
+	wg.Done()
+}
+
+// Third
+func cali(){
+	for i := 0; i < 30; i++ {
+		fmt.Println("cali:", i)
+		time.Sleep(time.Duration(2 * time.Millisecond))
+	}
+	wg.Done()
+}
