3be8a79cd495eafca30867358729bc2cfbf6fc77
bujosa <davidbujosa@gmail.com>
2022-11-11 20:28:28 -0400

    refactor: eliminate unused code in mutex

--- mutex/main.go
+++ mutex/main.go
@@ -8,15 +8,12 @@ import (
 	"time"
 )
 
-// I use this packages because i need to add more go routines
-// So I need to wait for this.
 var wg sync.WaitGroup
 var counter int
 var mutex sync.Mutex
 
 func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
-	fmt.Println(runtime.NumCPU())
 }
 
 func main() {
@@ -30,10 +27,8 @@ func main() {
 func incrementor(s string) {
 	for i := 0; i < 20; i++ {
 		time.Sleep(time.Duration(rand.Intn(20))*time.Millisecond)
-		mutex.Lock()
-		x := counter
-		x++
-		counter = x
+		mutex.Lock() // I use this because lock block the access 
+		counter++
 		mutex.Unlock()
 	}
 	wg.Done()
