31e55dec330f6282f29b3f1a18ffe06d21b49f98
bujosa <davidbujosa@gmail.com>
2022-11-14 01:09:14 -0400

    feat: finish example n to 1

--- channels/n-to-1/main.go
+++ channels/n-to-1/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	n := 10
+	c := make(chan int)
+	done := make(chan bool)
+
+	for i := 0; i < n; i++ {
+		go func() {
+			for i := 0; i < n; i++ {
+				c <- i
+			}
+			done <- true
+		}()
+	}
+
+	// How many functions I can put in the same channels? - No number for that
+	go func() {
+		for i := 0; i < n; i++ {
+			<-done
+		}
+		close(c)
+	}()
+
+	for n := range c {
+		fmt.Println(n)
+	}
 }
